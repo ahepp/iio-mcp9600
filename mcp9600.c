c3de9ee644f85592c085017d7b1cd5da92bbf5d5
Andrew.Hepp <andrew.hepp@ahepp.dev>
2023-01-08 13:32:28 -0800

    fix issues raised in v1 submission upstream

--- mcp9600.c
+++ mcp9600.c
@@ -13,7 +13,6 @@
 #include <linux/module.h>
 
 #include <linux/iio/iio.h>
-#include <linux/iio/sysfs.h>
 
 /* MCP9600 registers */
 #define MCP9600_HOT_JUNCTION 0x0
@@ -29,14 +28,6 @@ static const struct iio_chan_spec mcp9600_channels[] = {
 		.address = MCP9600_HOT_JUNCTION,
 		.info_mask_separate =
 			BIT(IIO_CHAN_INFO_RAW) | BIT(IIO_CHAN_INFO_SCALE),
-		.scan_index = 0,
-		.scan_type = {
-			.sign = 's',
-			.realbits = 16,
-			.storagebits = 16,
-			.shift = 0,
-			.endianness = IIO_BE,
-		},
 	},
 	{
 		.type = IIO_TEMP,
@@ -45,20 +36,13 @@ static const struct iio_chan_spec mcp9600_channels[] = {
 		.modified = 1,
 		.info_mask_separate =
 			BIT(IIO_CHAN_INFO_RAW) | BIT(IIO_CHAN_INFO_SCALE),
-		.scan_index = 1,
-		.scan_type = {
-			.sign = 's',
-			.realbits = 13,
-			.storagebits = 16,
-			.shift = 0,
-			.endianness = IIO_BE,
-		},
 	},
 	IIO_CHAN_SOFT_TIMESTAMP(2),
 };
 
 struct mcp9600_data {
 	struct i2c_client *client;
+	struct mutex read_lock; /* lock to prevent concurrent reads */
 };
 
 static int mcp9600_read(struct mcp9600_data *data,
@@ -67,8 +51,11 @@ static int mcp9600_read(struct mcp9600_data *data,
 	__be16 buf;
 	int ret;
 
+	mutex_lock(&data->read_lock);
 	ret = i2c_smbus_read_i2c_block_data(data->client, chan->address, 2,
 					    (u8 *)&buf);
+	mutex_unlock(&data->read_lock);
+
 	if (ret < 0)
 		return ret;
 	*val = be16_to_cpu(buf);
@@ -81,28 +68,21 @@ static int mcp9600_read_raw(struct iio_dev *indio_dev,
 			    int *val2, long mask)
 {
 	struct mcp9600_data *data = iio_priv(indio_dev);
-	int ret = -EINVAL;
+	int ret;
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		ret = iio_device_claim_direct_mode(indio_dev);
+		ret = mcp9600_read(data, chan, val);
 		if (ret)
 			return ret;
-
-		ret = mcp9600_read(data, chan, val);
-		iio_device_release_direct_mode(indio_dev);
-		if (!ret)
-			return IIO_VAL_INT;
-
-		break;
+		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_SCALE:
 		*val = 62;
 		*val2 = 500000;
-		ret = IIO_VAL_INT_PLUS_MICRO;
-		break;
+		return IIO_VAL_INT_PLUS_MICRO;
+	default:
+		return -EINVAL;
 	}
-
-	return ret;
 }
 
 static const struct iio_info mcp9600_info = {
@@ -119,7 +99,8 @@ static int mcp9600_probe(struct i2c_client *client)
 	if (ret < 0)
 		return ret;
 	if (ret != MCP9600_DEVICE_ID_MCP9600)
-		return -ENODEV;
+		dev_warn(&client->dev, "Expected ID %x, got %x\n",
+				MCP9600_DEVICE_ID_MCP9600, ret);
 
 	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*data));
 	if (!indio_dev)
@@ -127,29 +108,21 @@ static int mcp9600_probe(struct i2c_client *client)
 
 	data = iio_priv(indio_dev);
 	data->client = client;
+	mutex_init(&data->read_lock);
 
-	i2c_set_clientdata(client, indio_dev);
 	indio_dev->info = &mcp9600_info;
 	indio_dev->name = "mcp9600";
 	indio_dev->modes = INDIO_DIRECT_MODE;
 	indio_dev->channels = mcp9600_channels;
 	indio_dev->num_channels = ARRAY_SIZE(mcp9600_channels);
 
-	return iio_device_register(indio_dev);
+	return devm_iio_device_register(&client->dev, indio_dev);
 }
 
-static int mcp9600_remove(struct i2c_client *client)
-{
-	struct iio_dev *indio_dev;
-	printk(KERN_ALERT "mcp9600_remove\n");
-
-	indio_dev = i2c_get_clientdata(client);
-
-	iio_device_unregister(indio_dev);
-	return 0;
-}
-
-static const struct i2c_device_id mcp9600_id[] = { { "mcp9600" }, {} };
+static const struct i2c_device_id mcp9600_id[] = {
+	{ "mcp9600" },
+	{}
+};
 MODULE_DEVICE_TABLE(i2c, mcp9600_id);
 
 static const struct of_device_id mcp9600_of_match[] = {
@@ -164,7 +137,6 @@ static struct i2c_driver mcp9600_driver = {
 		.of_match_table = mcp9600_of_match,
 	},
 	.probe_new = mcp9600_probe,
-	.remove = mcp9600_remove,
 	.id_table = mcp9600_id
 };
 module_i2c_driver(mcp9600_driver);
